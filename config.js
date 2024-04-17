01a8735303c050c92417997306b27fc0e336a732
chithien12322 <90127844+chithien12322@users.noreply.github.com>
2024-04-17 15:34:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+module.exports = {
+  TOKEN: process.env.TOKEN || "MTIyOTc5NzQzNTgwMzQzOTE4Nw.GP0zN9.J31HtCCxbH5gPDfYIAR5f62_BnfCNX_5Z8x3kI",
+  PREFIX: process.env.PREFIX || "d",
+  OWNER_ID: process.env.OWNER_ID || "571542657378156545",
+  EMPTY_LEAVE: 60,
+  LEAVE_EMPTY: true,
+  LEAVE_FINISH: true,
+  LEAVE_STOP: true,
+}
